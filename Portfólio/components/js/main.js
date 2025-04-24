367d9c7342b27bfc23f5a7e23d4a07ee063abca6
Julio Cesar de Jesus Bonfim <159816707+Juliocesarsenai@users.noreply.github.com>
2025-04-24 22:16:41 +0000

    Salvar

--- Portfólio/components/js/main.js
+++ Portfólio/components/js/main.js
@@ -26,8 +26,61 @@ function updateSoftSkills(profileData) {
     .map((skills) => `<li>${skills}</li>`)
     .join("");
 }
+
+function updateHardSkills(profileData) {
+  const hardSkills = document.getElementById("profile.skills.hardSkills");
+  hardSkills.innerHTML = profileData.skills.hardSkills
+    .map(
+      (skill) =>
+        `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`
+    )
+    .join("");
+}
+
+function updateLanguages(profileData) {
+  const languages = document.getElementById("profile.languages");
+  languages.innerHTML = profileData.languages
+    .map((language) => `<li>${language}</li>`)
+    .join("");
+}
+
+function updatePortfolio(profileData) {
+  const portfolio = document.getElementById("profile.portfolio");
+  portfolio.innerHTML = profileData.portfolio
+    .map((project) => {
+      return `
+          <li>
+              <h3 ${project.github ? 'class = "github"' : ""}>${
+        project.name
+      }</h3>
+              <a href="${project.url}" target="_blank">${project.link}</a>
+          </li>
+      `;
+    })
+    .join("");
+}
+
+function updateFormations(profileData) {
+  const formacoes = document.getElementById("profileFormacoes");
+  formacoes.innerHTML = profileData.formacoes
+    .map((formacao) => {
+      return `
+        <li>
+            <h3 class="title">${formacao.name}</h3>
+            <p class="period">${formacao.period}</p>
+            <p>${formacao.description}</p>
+        </li>
+      `;
+    })
+    .join("");
+}
+
 (async () => {
   const profileData = await fetchProfileData();
   updateProfileInfo(profileData);
   updateSoftSkills(profileData);
+  updateHardSkills(profileData);
+  updateLanguages(profileData);
+  updatePortfolio(profileData);
+  updateFormations(profileData);
 })();
