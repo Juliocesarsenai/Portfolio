c6751c0282172e0275c5c25be0caa1f72f5aee64
Julio Cesar de Jesus Bonfim <159816707+Juliocesarsenai@users.noreply.github.com>
2025-04-25 00:55:05 +0000

    Salvar alterações finais

--- components/js/main.js
+++ components/js/main.js
@@ -1,16 +1,9 @@
 function updateProfileInfo(profileData) {
-  const photo = document.getElementById("profile-photo");
-  photo.src = profileData.photo;
-  photo.alt = profileData.name;
-
   const name = document.getElementById("profile-name");
   name.innerText = profileData.name;
 
   const perfil = document.getElementById("profile-job");
   perfil.innerText = profileData.job;
-
-  const location = document.getElementById("profile-local");
-  location.innerText = profileData.location;
 
   const phone = document.getElementById("profile-phone");
   phone.innerText = profileData.phone;
@@ -20,39 +13,36 @@ function updateProfileInfo(profileData) {
   email.innerText = profileData.email;
   email.href = `mailto:${profileData.email}`;
 }
-function updateSoftSkills(profileData) {
-  const softSkills = document.getElementById("profile.skills.softSkills");
-  softSkills.innerHTML = profileData.skills.softSkills
-    .map((skills) => `<li>${skills}</li>`)
-    .join("");
-}
 
-function updateHardSkills(profileData) {
-  const hardSkills = document.getElementById("profile.skills.hardSkills");
+function updateSkills(profileData) {
+  const hardSkills = document.getElementById("profile-skills-hardSkills");
   hardSkills.innerHTML = profileData.skills.hardSkills
     .map(
       (skill) =>
         `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`
     )
     .join("");
+
+  const softSkills = document.getElementById("profile-skills-softSkills");
+  softSkills.innerHTML = profileData.skills.softSkills
+    .map((skill) => `<li>${skill}</li>`)
+    .join("");
 }
 
 function updateLanguages(profileData) {
-  const languages = document.getElementById("profile.languages");
+  const languages = document.getElementById("profile-languages");
   languages.innerHTML = profileData.languages
     .map((language) => `<li>${language}</li>`)
     .join("");
 }
 
 function updatePortfolio(profileData) {
-  const portfolio = document.getElementById("profile.portfolio");
+  const portfolio = document.getElementById("profile-portfolio");
   portfolio.innerHTML = profileData.portfolio
     .map((project) => {
       return `
           <li>
-              <h3 ${project.github ? 'class = "github"' : ""}>${
-        project.name
-      }</h3>
+              <h3 ${project.github ? 'class="github"' : ""}>${project.name}</h3>
               <a href="${project.url}" target="_blank">${project.link}</a>
           </li>
       `;
@@ -61,7 +51,7 @@ function updatePortfolio(profileData) {
 }
 
 function updateFormations(profileData) {
-  const formacoes = document.getElementById("profileFormacoes");
+  const formacoes = document.getElementById("profile-formacoes");
   formacoes.innerHTML = profileData.formacoes
     .map((formacao) => {
       return `
@@ -78,8 +68,7 @@ function updateFormations(profileData) {
 (async () => {
   const profileData = await fetchProfileData();
   updateProfileInfo(profileData);
-  updateSoftSkills(profileData);
-  updateHardSkills(profileData);
+  updateSkills(profileData);
   updateLanguages(profileData);
   updatePortfolio(profileData);
   updateFormations(profileData);
